e390acf5e24fc28f54ee2b67b9ffeb4428ee6c3e
Hamza Haiken <hamza.haiken@ordina.nl>
2018-02-24 00:03:03 +0100

    Extract platform specific features

--- dnap/ui/app.py
+++ dnap/ui/app.py
@@ -16,24 +16,7 @@ def start(scheduler):
     app = Dnap(False)
 
     if sys.platform == "darwin":
-        from AppKit import NSUserDefaults
-        from .tray import TRAY_ICON_BLACK, TRAY_ICON_WHITE
-
-        def is_dark():
-            return NSUserDefaults.standardUserDefaults().stringForKey_("AppleInterfaceStyle") == "Dark"
-
-        dark = None
-
-        def update_icon():
-            nonlocal dark
-            if dark != is_dark():
-                dark = not dark
-                if dark:
-                    app.tray.set_icon(TRAY_ICON_WHITE)
-                else:
-                    app.tray.set_icon(TRAY_ICON_BLACK)
-
-
-        scheduler.add_job(update_icon, trigger="cron", second="*")
+        from .platforms import macos
+        scheduler.add_job(macos.update_icon, trigger="cron", args=[app.tray], second="*")
 
     app.MainLoop()
