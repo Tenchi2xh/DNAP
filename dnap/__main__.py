51c1b8aa4804be099998170a70f67ff2e28904a4
Hamza Haiken <hamza.haiken@ordina.nl>
2018-02-25 10:47:26 +0100

    Drop apscheduler in favor of wx timers

--- dnap/__main__.py
+++ dnap/__main__.py
@@ -1,18 +1,10 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-
 from .ui import app
-from .util import add_cron
 from .scraper.scrape import scrape
 
 
 def main():
-    scheduler = BackgroundScheduler()
-    scheduler.start()
-
-    #add_cron(scheduler, scrape_and_notify, {"minute": "*/15"})
     scrape(interval=15 * 60, verbose=True)
-
-    app.start(scheduler)
+    app.start()
 
 
 if __name__ == "__main__":
