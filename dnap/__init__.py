df22387c0099ba1a16123e826f4dac3bf78df988
Hamza Haiken <tenchi@team2xh.net>
2018-02-26 16:18:05 +0100

    Fix logging file unable to be created

--- dnap/__init__.py
+++ dnap/__init__.py
@@ -5,6 +5,8 @@ import builtins
 
 
 cache_path = os.path.expanduser("~/.dnap")
+if not os.path.isdir(cache_path):
+    os.mkdir(cache_path)
 cache_releases_path = os.path.join(cache_path, "releases.json")
 cache_result_path = os.path.join(cache_path, "result.json")
 cache_images_path = os.path.join(cache_path, "images")
